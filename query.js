4acc04bb8a63d9a60b03191c37fe3befd3b87eb9
Sm1thza <655021000980@mail.rmutk.ac.th>
2024-09-26 18:47:17 +0700

    Add files via upload

---
+++ query.js
@@ -0,0 +1,14 @@
+const client = require('./db');
+
+const getUsers = async () => {
+try {
+const res = await client.query('SELECT * FROM users');
+console.log('Users:', res.rows);
+} catch (err) {
+console.error('Error querying users:', err);
+} finally {
+client.end();
+}
+};
+getUsers();
+
